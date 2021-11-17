a8ad2f8904bc4f702704d95bc1185b7e9041ffdf
unknown <Volodymyr Goloida>
2021-11-17 19:01:37 +0200

    +posibilities+adaptiv+config

--- src/Components/Posibilities/Posibilities.jsx
+++ src/Components/Posibilities/Posibilities.jsx
@@ -4,15 +4,15 @@ import { possibilities } from "../config";
 import aplhabetIMG from "../../Images/Alphabet.svg";
 export const Posibilities = () => (
   <div className={styles.sectionPosibiilities}>
-    {possibilities.map((el, index) => {
+    {possibilities.map(({ title, text, img, color }, index) => {
       return (
         <div className={styles.containerPosibilities} key={index}>
-          <div className={styles.circle}>
-            <img src={aplhabetIMG} />
+          <div className={styles.circle} style={{ background: color }}>
+            <img src={img} />
           </div>
           <div className={styles.containerPosibilitiesText}>
-            <h1>{el.title}</h1>
-            <p>{el.text}</p>
+            <h1>{title}</h1>
+            <p>{text}</p>
           </div>
         </div>
       );
--- src/Components/config.js
+++ src/Components/config.js
@@ -1,3 +1,7 @@
+import aplhabetIMG from "../Images/LargeAlphaBet.svg";
+import shape from "../Images/Shape.svg";
+import stars from "../Images/stars.svg";
+import lamp from "../Images/lamp.svg";
 const navbarItems = [
   "Why",
   "About us",
@@ -6,6 +10,7 @@ const navbarItems = [
   "Testimonial",
   "FAQ"
 ];
+
 const featureStatistic = [
   {
     count: "8914+",
@@ -22,26 +27,26 @@ const featureStatistic = [
 ];
 const possibilities = [
   {
-    img: "",
-    class: "orng",
+    img: aplhabetIMG,
+    color: "#FF7D56",
     title: "Play while Learning",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a risus sit amet justo venenatis rhoncus vel sed dolor. "
   },
   {
-    img: "",
-    class: "grn",
+    img: shape,
+    color: "#B2D93A",
     title: "Designed for Growing brain",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a risus sit amet justo venenatis rhoncus vel sed dolor. "
   },
   {
-    img: "",
-    class: "bl",
+    img: stars,
+    color: "#6ED2EF",
     title: "Created by expert",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a risus sit amet justo venenatis rhoncus vel sed dolor. "
   },
   {
-    img: "",
-    class: "ylw",
+    img: lamp,
+    color: "#FDCF3F",
     title: "Building Children's Creativity",
     text: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Morbi a risus sit amet justo venenatis rhoncus vel sed dolor. "
   }
